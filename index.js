80bafb3fbdabbd81f38fd72f5c2ebf68447e801d
hevedrios <exxed.ridel@gmail.com>
2022-08-03 20:47:34 -0500

    Challenge: Online Users w/Flex

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
 /* 
-    Challenge:
-        
-        Reverse the Column's direction in my-gallery
-            Such that my-photos appear over #my-selected-photo
-        
-        Change the size of #my-selected-photo to 300x300
-            And set its default src to be the 3rd item in the Photos array
-            
-        Align the photos in my-photos to the right, using Flexbox
-            Align #my-selected-photo to the left, using Flexbox
+    Challenge
+    
+    Using the body tag as a Flexbox container
+        re-create the provided layout
+    
+    Hint: The Header and Footer are each 10% 
+        of the container's height
 */
 
-async function getPhotos() {
-   let response = await fetch("photos.json")
-   let photos = await response.json()
-   return photos
+async function getUsers() {
+  let response = await fetch("users.json");
+  let users = await response.json();
+  return users;
 }
 
-function getPhotosHtml(photos) {
-   let myPhotosHtml = photos.map(photo => {
-       return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
-   }).join('')
-   
-   return `<div class="my-photos">${myPhotosHtml}</div>`   
+function getUserDiv(user) {
+  return `<div class="my-online-user">
+        <div class="user-username">${user.username}</div>
+        <div class="user-online"></div>
+    </div>`;
 }
 
-getPhotos().then(photos => {    
-   document.body.innerHTML = `<div class="my-gallery">
-       <img class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/${photos[2].id}/300/300" />
-       ${getPhotosHtml(photos)}
-   </div>`    
-   
-   let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"))
-   myPhotoImgs.forEach(photoImg => {
-       photoImg.addEventListener("click", event => {
-           let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}300/300`
+getUsers().then((users) => {
+  let sampleUser = users[0];
 
-           let selectedPhoto = document.getElementById("my-selected-photo")
-           selectedPhoto.src = selectedPhotoSrc
-           selectedPhoto.style.display = "inline"
-       })
-   })
-})
+  document.body.innerHTML = `
+    <div class="header">Header</div>
+    <div class="my-online-users">
+        ${users.map((user) => getUserDiv(user)).join("")}
+    </div>
+    <div class="main-content">Main Content</div>
+    <div class="footer">Footer</div>
+    `;
+});
