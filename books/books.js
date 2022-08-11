e9a4aa15e356d425f04b8b37d75f70e0bbd4f32f
hevedrios <exxed.ridel@gmail.com>
2022-08-10 20:37:43 -0500

    Challenge: Library, flex grid catch, done

--- books/books.js
+++ books/books.js
@@ -1,56 +1,40 @@
 /* 
-    Library
-    
-    Fetch the collection of books 
-        from books.json, assign each 
-        a unique id
-        
-    Create a div to represent a book
-        using Grid & grid-template-areas 
-        build a book component
-        display all books using Flexbox
+    Library (Challenge)
+
+    Write a displayLibrary(books) function
+        - Add error catching
+
+    Use CSS Grid instead of Flexbox to style 
+        the library container
+        - Use a gap of 1rem
+        - Display 3 books per row
 */
 
-async function getBooksCollection() {
-  let response = await fetch("books.json");
-  let data = await response.json();
-  /* Adds ID++ to data json */
-  let n = 1;
-  console.log(data)
-  return data.map((book) => {
-    book.id = n;
-    n += 1;
-    return book;
-  });
+async function getBooks() {
+   let response = await fetch('books.json')
+   let books = await response.json()
+   let n = 1
+   return books.map(book => {
+       book.id = n
+       n += 1
+       return book
+   })
 }
 
-function getBookHTML(aBook) {
-  return `
-      <div class="a-book">
-         <div class="a-book-id">Id: ${aBook.id}</div>
-         <div class="a-book-author">${aBook.author}</div>
-         <div class="a-book-title">${aBook.title}</div>
-         <div class="a-book-wiki"> ${aBook.wiki}</div>
-      </div>
-   `;
+function getBookHtml(book) {
+   return `<div class="my-book">
+       <div class="my-book-cover">${book.title}</div>
+       <div class="my-book-spine"></div>
+       <div class="my-book-footer"></div>
+   </div>`
 }
 
-/* Better to read */
-function displayLibrary(allBooks) {
-  document.body.innerHTML = `
-      <div class="my-library">
-         ${allBooks.map(getBookHTML).join("")}
-      </div>
-   `;
+function displayLibrary(books) {
+   document.body.innerHTML = `<div class="my-library">
+       ${books.map(getBookHtml).join('')}
+   </div>`
 }
 
-getBooksCollection().then(displayLibrary);
-
-/* Switch commends */
-// getBooksCollection().then(allBooks => {
-//    document.body.innerHTML = `
-//       <div class="my-library">
-//          ${allBooks.map(getBookHTML).join("")}
-//       </div>
-//    `;
-// });
+getBooks()
+   .then(displayLibrary)
+   .catch(e => console.log(e))
